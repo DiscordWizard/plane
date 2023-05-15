5c00d1bfa3eb8c31862206200d55fb542838fe49
DiscordWizard <pausakime.if@gmail.com>
2023-05-15 19:23:19 +0200

    Canvi integer -> Binari

--- models/avio.py
+++ models/avio.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 class avio(models.Model):
     _name = 'avio'
     codi = fields.Integer('Codi', required=True)
-    imatge = fields.Char('Imatge')
+    imatge = fields.Binary('Imatge')
     marca = fields.Integer('Marca')
     model = fields.Char('Model')
     maxVel = fields.Float('MaxVel')
