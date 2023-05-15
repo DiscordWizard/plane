f05c4933cbb3299c092744809941dc4d42cbb588
DiscordWizard <pausakime.if@gmail.com>
2023-05-15 19:30:31 +0200

    Canvi Char -> Integer

--- models/vol.py
+++ models/vol.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 class vol(models.Model):
     _name = 'vol'
     codi = fields.Integer('Codi', required=True)
-    passatgers = fields.Char('Passatgers')
+    passatgers = fields.Integer('Passatgers')
     dataSortida = fields.Date('DataSortida')
     dataArrivada = fields.Date('DataArrivada')
     desti = fields.Many2one('aeroport',string='Desti')
